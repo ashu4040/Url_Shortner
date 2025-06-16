f26a9cf2673027f29611fd5bd2cbce166f42848d
Ayush Kumar <ak433778@gmail.com>
2025-06-16 17:53:44 +0530

    updates

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://url-shortner-backend-9chw.onrender.com",
-  timeout: 10000, //10s
   withCredentials: true,
 });
 
